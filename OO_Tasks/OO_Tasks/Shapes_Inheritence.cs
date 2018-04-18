217dc77390701fd33a2c7b0abd96acac8620b4ef
thaceul <33332888+thaceul@users.noreply.github.com>
2018-04-18 15:14:02 +1000

    Add files via upload

---
+++ OO_Tasks/OO_Tasks/Shapes_Inheritence.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace OO_Tasks_Inheritence
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+
+        public Shape(string name, string colour)
+        {
+            Name = name;
+            Colour = colour;
+        }
+
+        public abstract double GetPerimeter();
+
+        public abstract double GetArea();
+    }
+
+    public abstract class Quadrilateral : Shape
+    {
+        public int NumSides { get; set; } = 4;
+
+        public Quadrilateral(string name, string colour) : base(name, colour) { }
+    }
+
+    public class Square : Shape
+    {
+        private double SideLength { get; set; } = 4;
+
+        public Square(string name, string colour, double sideLength) : base(name, colour)
+        {
+            SideLength = sideLength;
+        }
+
+        public override double GetPerimeter()
+        {
+            return Math.Round (4 * SideLength,2);
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round (SideLength * SideLength,2);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double Length { get; set; }
+        private double Width { get; set; }
+
+        public Rectangle(string name, string colour, double length, double width) : base(name, colour)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double GetPerimeter()
+        {
+            return Math.Round(2 * (Length + Width), 2);
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round(Length * Width, 2);
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public const double PI = 3.142;
+
+        public Circle(string name, string colour, double radius) : base(name, colour)
+        {
+            Radius = radius;
+        }
+
+        public override double GetPerimeter()
+        {
+            return GetCircumference();
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round ( PI * Math.Pow(Radius, 2), 2);
+        }
+
+        public double GetCircumference()
+        {
+            return Math.Round (2 * PI * Radius, 2);
+        }
+    }
+}
---
+++ OO_Tasks/OO_Tasks/Shapes_Interface.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace OO_Tasks_Interface
+{
+    public interface IShapeProps
+    {
+        double GetArea();
+        double GetPerimeter();
+    }
+
+    public interface ICircleProps
+    {
+        double GetArea();
+        double GetCircumference();
+    }
+
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+
+        public Shape(string name, string colour)
+        {
+            Name = name;
+            Colour = colour;
+        }
+    }
+
+    public abstract class Quadrilateral : Shape
+    {
+        public int NumSides { get; set; } = 4;
+
+        public Quadrilateral(string name, string colour) : base(name, colour) { }
+    }
+
+    public class Square : Shape, IShapeProps
+    {
+        private double _sideLength;
+
+        public Square(string name, string colour, double sideLength) : base(name, colour)
+        {
+            _sideLength = sideLength;
+        }
+
+        public double GetPerimeter()
+        {
+            return 4 * _sideLength;
+        }
+
+        public double GetArea()
+        {
+            return Math.Round(Math.Pow(_sideLength, 2),2);
+       
+        }
+    }
+
+    public class Rectangle : Shape, IShapeProps
+    {
+        private double _length, _width;
+
+        public Rectangle(string name, string colour, double length, double width) : base(name, colour)
+        {
+            _length = length;
+            _width = width;
+        }
+
+        public double GetPerimeter()
+        {
+            return Math.Round (2 * (_length + _width),2);
+        }
+
+        public double GetArea()
+        {
+            return Math.Round (_length * _width, 2);
+        }
+    }
+
+    public class Circle : Shape, ICircleProps
+    {
+        public double Radius { get; set; }
+        public const double PI = 3.142f;
+
+        public Circle(string name, string colour, double radius) : base(name, colour)
+        {
+            Radius = radius;
+        }
+
+       public double GetPerimeter()
+       {
+            return GetCircumference();
+       }
+
+        public double GetArea()
+        {
+            return Math.Round(PI * Radius * Radius,2);
+        }
+
+        public double GetCircumference()
+        {
+            return Math.Round ( 2 * PI * Radius, 2);
+        }
+    }
+}
